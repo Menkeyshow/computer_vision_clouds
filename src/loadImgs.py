ae63e11bf409fd14db75400b5859b85d31004c33
Alipacman <ali_pourasad@yahoo.de>
2018-06-16 12:03:39 +0200

    add loadImgs script

---
+++ src/loadImgs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 16 10:55:11 2018
+
+@author: ali
+"""
+
+import glob
+from skimage.io import imread, imsave, imshow
+import PIL
+from PIL import Image
+        
+        
+altocumulus_path = "../data/altocumulus/*"
+altocumulus_pics = glob.glob(altocumulus_path)
+
+altostratus_path = "../data/altostratus/*"
+altostratus_pics = glob.glob(altostratus_path)
+
+cirrocumulus_path = "../data/cirrocumulus/*"
+cirrocumulus_pics = glob.glob(cirrocumulus_path)
+
+cirrostratus_path = "../data/cirrostratus/*"
+cirrostratus_pics = glob.glob(cirrostratus_path)
+
+cirrus_path = "../data/cirrus/*"
+cirrus_pics = glob.glob(cirrus_path)
+
+cumulonimbus_path = "../data/cumulonimbus/*"
+cumulonimbus_pics = glob.glob(cumulonimbus_path)
+
+cumulus_path = "../data/cumulus/*"
+cumulus_pics = glob.glob(cumulus_path)
+
+nimbostratus_path = "../data/nimbostratus/*"
+nimbostratus_pics = glob.glob(nimbostratus_path)
+
+stratocumulus_path = "../data/stratocumulus/*"
+stratocumulus_pics = glob.glob(stratocumulus_path)
+
+stratus_path = "../data/stratus/*"
+stratus_pics = glob.glob(stratus_path)
+
+stratiform = []
+stratiform = cirrostratus_pics + altostratus_pics + nimbostratus_pics + stratus_pics
+
+cirriform = []
+cirriform = cirrus_pics
+
+stratocumuliform = []
+stratocumuliform = cirrocumulus_pics + altocumulus_pics + stratocumulus_pics
+
+cumuliform = []
+cumuliform = cumulus_pics + cumulonimbus_pics
+
+
+
+def resize_pics(array,height, widt, foldername):
+    '''function to resize pics to a size''' 
+    counter = 0
+    for element in array:
+        img =  Image.open(element)    
+        resized = img.resize((height, widt), PIL.Image.ANTIALIAS)
+        path = "../temp/%s/photo%s.jpg" % (foldername, counter)
+        resized.save(path)
+        counter += 1
+        
+
+resize_pics(stratiform, 500, 500, "stratiform")
+resize_pics(cirriform, 500, 500, "cirriform")
+resize_pics(stratocumuliform, 500, 500, "stratocumuliform")
+resize_pics(cumuliform, 500, 500, "cumuliform")
+
+
+
