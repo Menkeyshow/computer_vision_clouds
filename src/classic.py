66d67ad2ce704881deaebb0d504a5c80804e2139
Maximilian Birkenhagen <6birkenh@informatik.uni-hamburg.de>
2018-07-05 16:58:38 +0200

    added first (not yet working) cropped images prototype

--- src/classic.py
+++ src/classic.py
@@ -1,6 +1,11 @@
 import prepareData
 from prepareData import trainData, trainLabels, valiData, valiLabels
 import numpy as np
+import box_clouds as box
+import PIL
+from PIL import Image
+from skimage.io import imread, imsave, imshow
+
 
 """ 
 trainData = [] #das ist gegeben
@@ -9,6 +14,11 @@ valiData = []  #das ist gesucht
 trainLabels = []
 valiLabels = []
 """
+
+bin_trainData = []
+bin_valiData = []
+
+
 trMerkmale = [] 
 vaMerkmale = []
 trMerkmale2 = [] 
@@ -18,8 +28,9 @@ vaMerkmale3 = []
 trMerkmale4 = []
 vaMerkmale4 = []
 
-nbins=7
+nbins=4
 
+    
 def getMerkmal(img, Merkmal, nbins):
     if Merkmal == 'mean':
         return np.mean(img, axis=(0,1))
@@ -33,11 +44,30 @@ def getMerkmal(img, Merkmal, nbins):
     if Merkmal == 'histogram3D':
         imgReshaped = img.reshape((img.shape[0]*img.shape[1],3)) #Reshapen, damit jedes Pixek in einer Zeile liegt
         return np.histogramdd(imgReshaped, bins = [nbins,nbins,nbins], range=((0,256),(0,256),(0,256)))[0].flatten()
+    
+''' 
+funktioniert noch nicht so ganz ....
+def create_cropped_images():
+    for img in trainData:
+        image = box.binarized_crop(img, 0.2)
+        image = image.resize((500, 500), PIL.Image.ANTIALIAS)
+        bin_trainData.append(img[image.shape[0], image.shape[1],:])
+        
+    for img in valiData:
+        image = box.binarized_crop(img, 0.2)
+        bin_valiData.append(img[image.shape[0], image.shape[1],:])
+    trainData = bin_trainData
+    valiData = bin_valiData
+'''
 
 #TODO: binarisierung einbauen, sonst macht das Histogramm noch nicht so viel Sinn...
+
+#MAIN PROGRAMM
 if __name__ == '__main__':
-    prepareData.prepare_data() #speichert Daten in trainData, trainLabels, valiData, valiLabels
-    
+    #speichert Daten in trainData, trainLabels, valiData, valiLabels
+    prepareData.prepare_data() 
+
+    #berechnet Merkmale in den zugehörigen Arrays
     for img in trainData: 
         trMerkmale.append(getMerkmal(img, 'histogram3D', nbins))
         trMerkmale2.append(getMerkmal(img, 'std', 0))
@@ -50,8 +80,8 @@ if __name__ == '__main__':
         
     #Gewichte
     W0=1
-    W1=50
-    W2=50
+    W1=0.7
+    W2=0.7
     result = []
     
     #Distanzvergleich
@@ -62,6 +92,7 @@ if __name__ == '__main__':
                              W1*np.linalg.norm(vaM2-trM2)+
                              W2*np.linalg.norm(vaM3-trM3)) 
         result.append(trainLabels[np.argmin(distances)]) 
+
 
     #Accuracy Berechnung
     correct = 0.0
