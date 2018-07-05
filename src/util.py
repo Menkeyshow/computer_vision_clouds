cf342ad1772b5b84cae35ce6db98671e3fd9b86e
Maximilian Birkenhagen <6birkenh@informatik.uni-hamburg.de>
2018-07-05 23:40:05 +0200

    added cropImageArray - which will later be used to replace create_cropped_images in classic.py

--- src/util.py
+++ src/util.py
@@ -1,4 +1,7 @@
 import os
+import skimage
+#own modules
+import box_clouds as box
 
 def ensure_directory_exists(dirname):
     """Checks if the directory `dirname` exists, and creates it if it
@@ -21,3 +24,19 @@ cloud_kinds = {
     "stratocumuliform": ["cirrocumulus", "altocumulus", "stratocumulus"],
     "cumuliform": ["cumulus", "cumulonimbus"]
 }
+
+def cropImageArray(array):
+    '''
+    Returns a cropped imageArray, cropped with the box_clouds method.
+    '''
+    cropped_array = []
+    for img in array:
+        image_shape = box.binarized_crop(img, 0.2)
+        #print("image_shape:" ,image_shape)
+        #print("img_shape:" , img.shape)
+        image = img[0:image_shape[0],0:image_shape[1],:]
+        #print("image:",image.shape)
+        resized = skimage.transform.resize(image, (500,500))
+        #print("resized:",resized.shape)
+        cropped_array.append(resized)
+        return cropped_array
