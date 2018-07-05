9f1df01fe29d857469ab5e7863c421c0eb0f002a
Maximilian Birkenhagen <6birkenh@informatik.uni-hamburg.de>
2018-07-05 23:37:27 +0200

    fixed binarized_crop to acttually do what is advertised

--- src/box_clouds.py
+++ src/box_clouds.py
@@ -18,45 +18,53 @@ import glob
 import pdb
 import skimage.color
 
-''' Aufgabe 2 '''
-
-plt.close("all")
-
-img = glob.glob("../temp/cirriform/cirriform29.jpg")
-for x in img:
-    pic = imread(x)
-
-# 2.1
-# Binarize image with every value mean within the threshold being in the foreground
 def binarize(img):
+    '''
+    Binarize an image via its greyscaled mean value.
+    '''
     pic = skimage.color.rgb2grey(img)
     image_mean = np.mean(pic) 
-    print(image_mean,np.max(pic),pic.shape)
+    #print(image_mean,np.max(pic),pic.shape)
     if (image_mean > .4):
         image_mean = 0.03
     return pic > image_mean
     
 
 def cut_black_out(img, thresh): #wir nehmen 0.2 als treshold?
+    '''
+    Cut out the bottom of the image which is presumably blacker than the top.
+    '''
+    box_not_cloud= True
     dont_crop = False
-    box_height = np.shape(img)[0] * 0.03
-    bool_= True
+    box_height = np.shape(img)[0] * 0.03 #3% des Bildes werden als Box getestet
     y = np.shape(img)[0]
-    while(bool_):
+    while(box_not_cloud):
         image_box = img[int(y) - int(box_height) : int(y),:]
         box_mean = np.mean(image_box)
         y -= box_height/2
-        print (y,box_height)
         if y < 0:
-            bool_ = False
+            box_not_cloud = False
             dont_crop = True
         if box_mean > thresh:
-            bool_ = False
+            box_not_cloud = False
     if dont_crop:
         return img
     return img[0:int(y),:]
-        
-def binarized_crop(img,thresh):
-    pic1 = binarize(img).astype(np.float)
-    pic2 = cut_black_out(pic1, 0.2)
-    return pic2.shape #??
+
+def binarized_crop(img, thresh):
+    '''
+    Returns the shape an image has to be cropped to cut out non-sky objects.
+    Use 0.2 as a treshold.
+    '''
+    binarized_img = binarize(img).astype(np.float)
+    cropped_img = cut_black_out(binarized_img, 0.2)
+    return cropped_img.shape
+
+''' debug stuff
+
+plt.close("all")
+
+img = glob.glob("../temp/classic/cirriform/cirriform29.jpg")
+for x in img:
+    pic = imread(x)
+'''
